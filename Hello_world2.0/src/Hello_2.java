4bbe090f55501a8df9b15b3ca98e2b6fe9604cbf
FoyArthur <20FoyA06@stu.smuhsd.org>
2018-08-22 09:37:15 -0700

    First program to commit

---
+++ Hello_world2.0/src/Hello_2.java
@@ -0,0 +1,17 @@
+
+public class Hello_2 {
+
+		
+		/**
+		 * Prints out a message saying Hello!
+		 * 
+		 * @author Arthur
+		 * @version 1.1
+		 */
+		
+
+		    public static void main(String[] args){
+		    	
+		        System.out.println("Hello World! From Arthur Foy"); //Display the string
+		    }
+}
