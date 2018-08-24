8e648df67318af373545baa77d568532cc62d626
FoyArthur <20FoyA06@stu.smuhsd.org>
2018-08-24 09:01:24 -0700

    changed name

--- Hello_world2.0/src/Hello_2.java
+++ Hello_world2.0/src/Hello_2.java
@@ -5,13 +5,13 @@ public class Hello_2 {
 		/**
 		 * Prints out a message saying Hello!
 		 * 
-		 * @author Arthur
-		 * @version 1.1
+		 * @author Arthur Foy
+		 * @version 1.1  8/22/18
 		 */
 		
 
 		    public static void main(String[] args){
 		    	
-		        System.out.println("Hello World! From Arthur Foy"); //Display the string
+		        System.out.println("Hello World! From Arthur Foyy"); //Display the string
 		    }
 }
