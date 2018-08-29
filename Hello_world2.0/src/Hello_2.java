bccb5cd38159e272d77d3cb4a83c1999ded8dac6
FoyArthur <20FoyA06@stu.smuhsd.org>
2018-08-29 09:34:59 -0700

    go

--- Hello_world2.0/src/Hello_2.java
+++ Hello_world2.0/src/Hello_2.java
@@ -1,17 +1,12 @@
+/**
+* Prints out a message saying Hello!
+* 
+* @author Arthur Foy
+* @version 1.1  8/22/18
+* */
 
 public class Hello_2 {
-
-		
-		/**
-		 * Prints out a message saying Hello!
-		 * 
-		 * @author Arthur Foy
-		 * @version 1.1  8/22/18
-		 */
-		
-
 		    public static void main(String[] args){
-		    	
 		        System.out.println("HELLO World!! From Arthur Foy"); //Display the string
 		    }
 }
