e6b72e30fae264ea0bdb395b89a46dd865c92472
FoyArthur <42453633+FoyArthur@users.noreply.github.com>
2018-08-29 17:19:09 -0700

    Update Hello_2.java

--- Hello_world2.0/src/Hello_2.java
+++ Hello_world2.0/src/Hello_2.java
@@ -1,15 +1,12 @@
-
-public class Hello_2 {
-
-		
 		/**
 		 * Prints out a message saying Hello!
 		 * 
 		 * @author Arthur Foy
 		 * @version 1.1  8/22/18
 		 */
-		
 
+public class Hello_2 {
+	
 		    public static void main(String[] args){
 		    	
 		        System.out.println("HELLO World!! From Arthur Foy"); //Display the string
