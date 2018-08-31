96718fba98ade047f40a66a87f1540da6543c628
FoyArthur <20FoyA06@stu.smuhsd.org>
2018-08-31 09:50:19 -0700

    commit

---
+++ Pickachu/src/pika.java
@@ -0,0 +1,11 @@
+
+public class pika {
+
+	public static void main(String[] args) {
+		System.out.println("\t\t\t\tPikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t\t\t\t\t(\\_/)");
+		System.out.println("\t\t\t\t\t\t(o^.^)");
+		System.out.println("\t\t\t\t\t      (z(_(\")(\")");
+	}
+
+}
