d978def1ff6447fb986026ef83f15e6d5c246cf4
FoyArthur <20FoyA06@stu.smuhsd.org>
2018-10-31 10:46:36 -0700

    c

--- Pickachu/src/pika.java
+++ Pickachu/src/pika.java
@@ -1,11 +1,15 @@
-
+/* This code prints out text then draws picture
+ * Author: Arthur Foy
+ * 1.1
+ * 8/31/18
+ */ 
 public class pika {
 
 	public static void main(String[] args) {
-		System.out.println("\t\t\t\tPikachu welcomes you to the world of Pokemon!");
-		System.out.println("\t\t\t\t\t\t(\\_/)");
+		System.out.println("\t\t\t  Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t\t\t\t\t(\\__/)");
 		System.out.println("\t\t\t\t\t\t(o^.^)");
-		System.out.println("\t\t\t\t\t      (z(_(\")(\")");
+		System.out.println("\t\t\t\t\t     (z(_(\")(\")");
 	}
 
 }
