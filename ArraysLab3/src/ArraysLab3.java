c302680dd4fa48cac03ed75aec4695895b642e75
FoyArthur <20FoyA06@stu.smuhsd.org>
2018-11-14 10:46:46 -0800

    8

--- ArraysLab3/src/ArraysLab3.java
+++ ArraysLab3/src/ArraysLab3.java
@@ -1,14 +1,20 @@
 import java.util.Arrays;
 public class ArraysLab3 {
 	public static void main(String[] args) {
-		int[] arr5 = {1,2,3,4,5,6};
-		int[] arr = sum(arr5, arr5);
-		System.out.println(Arrays.toString(arr));
-		int[] arr321 = append(arr5, 5);
-		System.out.println(Arrays.toString(arr321));
-		int [] arr43 = remove(arr5, 1);
-		System.out.println(Arrays.toString(arr43));
-		System.out.println(sumEven(arr5));
+		int [] a1 = {5, 10, 15, 20, 25, 30, 35, 40} ;
+		int [] a2 = {7, 14, 21, 28, 35, 42, 49, 56};
+		int [] sumArr = sum(a1, a2);
+		int appendNum = 200;
+		int[] appendArr = append(a1, appendNum);
+		int removeIdx = 5;
+		int [] removeArr = remove(a2, removeIdx);
+		int sumOfEvens = sumEven(appendArr);
+		rotateRight(a1);
+		System.out.println(Arrays.toString(sumArr));
+		System.out.println(Arrays.toString(appendArr));
+		System.out.println(Arrays.toString(removeArr));
+		System.out.println(sumOfEvens);
+		System.out.println(Arrays.toString(a1));
 	}
 	
 	public static int[] sum(int[] arr1, int[] arr2) {
@@ -47,6 +53,15 @@ public class ArraysLab3 {
 			sum += arr[i];
 		}
 		return(sum);
+	}
+	
+	public static void rotateRight(int[] arr) {
+		int firstArr = arr[arr.length-1];
+		for(int i = arr.length-1; i > 0; i--) {
+			arr[i] = arr[i-1];
+		}
+		arr[0] = firstArr;
+		
 	}
 
 }
